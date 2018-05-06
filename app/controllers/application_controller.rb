d82dfd722a7d693155e12d0916ed4d5b6c449c90
Morgan Fudge <mfudge92@gmail.com>
2018-05-06 08:09:24 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     req.params['redirect_uri'] = "http://localhost:3000/auth"
     req.params['code'] = params[:code]
   end
- 
+
   body = JSON.parse(resp.body)
   session[:token] = body["access_token"]
   redirect_to root_path
